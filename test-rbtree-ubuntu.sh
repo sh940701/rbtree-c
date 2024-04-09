12c878ad478ccb02ab80d6969da04a1760c6ee0f
sh940701 <visioner2168@gmail.com>
2024-04-10 01:02:48 +0900

    update: invalid path

--- test-rbtree-ubuntu.sh
+++ test-rbtree-ubuntu.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
-# bstree-test 폴더의 Makefile에서 clean 실행
-cd bstree-test
+# test 폴더의 Makefile에서 clean 실행
+cd test
 make clean
 
-# bstree-src 폴더의 Makefile에서 clean 실행
-cd ../bstree-src
+# src 폴더의 Makefile에서 clean 실행
+cd ../src
 make clean
 
-# bstree-test 폴더로 돌아가서 test 실행
-cd ../bstree-test
+# test 폴더로 돌아가서 test 실행
+cd ../test
 make test
